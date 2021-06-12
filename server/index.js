bfe578b08746a5c976e1a925ce275b71405a9b72
Kishankumar Patel <pkishan2207@gmail.com>
2021-06-12 17:00:06 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ app.use(cors());
 app.use('/posts', postRoutes);
 app.use("/user", userRouter);
 
-const CONNECTION_URL = 'mongodb+srv://admin:admin@cluster0.mogev.mongodb.net/Memories?retryWrites=true&w=majority';
+const CONNECTION_URL = 'mongodb+srv://js_mastery:123123123@practice.jto9p.mongodb.net/test';
 const PORT = process.env.PORT|| 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
